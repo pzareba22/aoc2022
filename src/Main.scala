52ef4c80100a2b0c2e26b01f03e8d247703f07d3
pawelzareba <pzareba22@gmail.com>
2022-12-07 02:44:23 +0100

    Solved day 2

--- src/Main.scala
+++ src/Main.scala
@@ -2,6 +2,6 @@ package pzareba.aoc2022
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println(Solutions.day1())
+    println(Solutions.day2())
   }
 }
--- src/Solutions.scala
+++ src/Solutions.scala
@@ -19,7 +19,58 @@ object Solutions {
     (res1, res2)
   }
 
-  def day2(): Int = ???
+  def day2(): (Int, Int) = {
+    sealed trait Shape extends Product with Serializable
+    object Shape {
+      def apply(chr: Char): Shape = chr match {
+        case 'A' | 'X' => Rock
+        case 'B' | 'Y' => Paper
+        case 'C' | 'Z' => Scissors
+        case _ => throw new IllegalArgumentException("Invalid input character")
+      }
+    }
+    case object Rock extends Shape
+    case object Paper extends Shape
+    case object Scissors extends Shape
+    val values = Map(
+      Rock -> 1,
+      Paper -> 2,
+      Scissors -> 3,
+    )
+
+    val wins = Map(
+      Rock -> Scissors,
+      Paper -> Rock,
+      Scissors -> Paper,
+    )
+
+    def calculateScore(char1: Char, char2: Char): Int = {
+      val shape1 = Shape(char1)
+      val shape2 = Shape(char2)
+      val loosingShape = wins(shape1)
+      val score = shape2 match {
+        case _ if shape2 == loosingShape => 0 // loss
+        case _ if shape1 == shape2 => 3 // draw
+        case _ => 6 // win
+      }
+      values(shape2) + score
+    }
+
+    val contents = getContents("day2.txt").map(_.toCharArray).map {
+      case Array(c1, _, c2) => (c1, c2)
+      case _ => throw new IllegalStateException("Invalid input")
+    }
+    val res1 = contents.map(x => calculateScore(x._1, x._2)).sum
+    val res2 = contents.map {
+      case (c1, c2) => (c1, c2) match {
+        case (c1, 'X') => 0 + values(wins(Shape(c1))) // loss
+        case (c1, 'Y') => 3 + values(Shape(c1)) // draw
+        case (c1, 'Z') => 6 + values(wins.map(_.swap)(Shape(c1))) // win
+      }
+    }.sum
+
+    (res1, res2)
+  }
 
   private def getContents(fileName: String): Seq[String] = {
     val source = Source.fromFile("inputs/" + fileName)
